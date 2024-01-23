79689102a8911055a94da252e8dc9dde319689d4
SSeries <63185402+S-Series@users.noreply.github.com>
2024-01-23 16:05:31 +0900

    2024.01.23 || 16:05

---
+++ FileMerger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+
+namespace FileMerger
+{
+    public class Merger
+    {
+        public static void Main()
+        {
+            string path;
+            path = Directory.GetCurrentDirectory();
+            Console.WriteLine(path);
+        }
+    }
+}
